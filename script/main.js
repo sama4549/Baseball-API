b6926a1539069f1a812cf0d93c045744a95e4cbc
Sam Anderson <anderson.samuel61@gmail.com>
2021-08-25 22:34:30 -0500

    initial commit with basics of project completed

---
+++ script/main.js
@@ -0,0 +1,258 @@
+// Event Listeners
+window.addEventListener('DOMContentLoaded', fetchData);
+
+// Variables
+const pitcherTable = document.getElementById('pitcher-table');
+
+// Fetch Baseball data
+function fetchData() {
+    fetch("http://lookup-service-prod.mlb.com/json/named.roster_40.bam?team_id='142'")
+    .then(response => {
+        if(response.status !== 200) {
+            console.log('Something went wrong')
+        } else {
+            return response.json()
+        }
+    })
+    .then(data => {
+        console.log('40 Man Roster' , data);
+        obtainPitchers(data);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+}
+
+// Obtain List of Players
+function obtainPitchers(data) {
+    let playersArray = data.roster_40.queryResults.row;
+    playersArray.forEach(player => {
+        let name = player.name_display_first_last;
+        let position = player.position_txt;
+        let college = player.college;
+        let number = player.jersey_number;
+        let id = player.player_id;
+        let status = player.status_code;
+
+        if(position !== 'P') {
+            return;
+        }
+
+        // Generate table row
+        let newRow = document.createElement('tr');
+        newRow.className = 'player-row';
+        newRow.setAttribute('data-playerid', id);
+        newRow.addEventListener('click', pitcherDetails);
+
+        // Generate Player Name
+        let playerName = document.createElement('td');
+        playerName.appendChild(document.createTextNode(name));
+        newRow.appendChild(playerName);
+
+        // Generate Player Status
+        let playerStatus = document.createElement('td');
+        let statusTitle
+        switch(status) {
+            case 'A':
+                statusTitle = 'Active';
+                break;
+            case 'D10':
+                statusTitle = '10-day injured list';
+                break;
+            case 'ML':
+                statusTitle = 'Maternity Leave';
+                break;
+            case 'RM':
+                statusTitle = 'No official stats';
+                break;
+            default:
+                statusTitle = status;
+        }
+        playerStatus.appendChild(document.createTextNode(statusTitle));
+        newRow.appendChild(playerStatus);
+
+        // Generate Player Position
+        let playerPosition = document.createElement('td');
+        playerPosition.appendChild(document.createTextNode(position));
+        newRow.appendChild(playerPosition);
+
+        // Generate Player College
+        let playerCollege = document.createElement('td');
+        playerCollege.appendChild(document.createTextNode(college));
+
+        if(college === "") {
+            let noData = document.createElement('td');
+            noData.appendChild(document.createTextNode(" ' ' "))
+            newRow.appendChild(noData);
+        } else {
+            newRow.appendChild(playerCollege);
+        }
+
+
+        // Generate Player Number
+        let playerNumber = document.createElement('td');
+        playerNumber.appendChild(document.createTextNode(number));
+        
+        if(number === "") {
+            let noData = document.createElement('td');
+            noData.appendChild(document.createTextNode(" ' ' "))
+            newRow.appendChild(noData);
+        } else {
+            newRow.appendChild(playerNumber);
+        }
+
+        // Generate Player Record
+
+        // Obtain Pitching Stats
+        obtainPitchingStats(id).then(response => {
+            let playerRecord = document.createElement('td');
+
+            if (response.sport_pitching_tm.queryResults.totalSize === "0") {
+                playerRecord.appendChild(document.createTextNode('No Major League Activity'));
+            } else if (response.sport_pitching_tm.queryResults.totalSize === "2" || response.sport_pitching_tm.queryResults.totalSize === "3") {
+                let playerRecordSpan = document.createElement('span');
+                playerRecord.appendChild(playerRecordSpan);
+
+                playerRecordSpan.appendChild(document.createTextNode(`${response.sport_pitching_tm.queryResults.row[Number(response.sport_pitching_tm.queryResults.totalSize) - 1].w} / ${response.sport_pitching_tm.queryResults.row[Number(response.sport_pitching_tm.queryResults.totalSize) - 1].l}`))
+                playerRecord.appendChild(document.createTextNode(` (Acquired from trade with ${response.sport_pitching_tm.queryResults.row[Number(response.sport_pitching_tm.queryResults.totalSize) - 2].team_full})`));
+            } else {
+                let wins = response.sport_pitching_tm.queryResults.row.w;
+                let losses = response.sport_pitching_tm.queryResults.row.l;
+                playerRecord.appendChild(document.createTextNode(`${wins} / ${losses}`));
+            }
+
+            newRow.appendChild(playerRecord);
+        })
+
+        // Generate Player Pitching ERA
+
+        // Obtain Player Pitching Stats
+        obtainPitchingStats(id).then(response => {
+            let playerPitchingAverage = document.createElement('td');
+
+            // Get ERA from results data
+
+            // If Player has not played in the majors yet this year
+            if (response.sport_pitching_tm.queryResults.totalSize === "0") {
+                playerPitchingAverage.appendChild(document.createTextNode('No Major League Stats'));
+
+                // If Player Acquired By Another Team Mid-Season
+            } else if (response.sport_pitching_tm.queryResults.totalSize === "2" || response.sport_pitching_tm.queryResults.totalSize === "3") {
+                let playerPitchingAverageSpan = document.createElement('span');
+                playerPitchingAverage.appendChild(playerPitchingAverageSpan);
+
+                playerPitchingAverageSpan.appendChild(document.createTextNode(`${response.sport_pitching_tm.queryResults.row[Number(response.sport_pitching_tm.queryResults.totalSize) - 1].era}`))
+                playerPitchingAverage.appendChild(document.createTextNode(` (Acquired from trade with ${response.sport_pitching_tm.queryResults.row[Number(response.sport_pitching_tm.queryResults.totalSize) - 2].team_full})`));
+
+                // Get ERA Color
+                let returedColor = colorEra(response.sport_pitching_tm.queryResults.row[Number(response.sport_pitching_tm.queryResults.totalSize) - 1].era);
+                playerPitchingAverageSpan.style.color = returedColor;
+                playerPitchingAverageSpan.classList.add('player-era');
+
+            } else {
+                playerPitchingAverage.appendChild(document.createTextNode(response.sport_pitching_tm.queryResults.row.era));
+
+                // Get ERA Color
+                let returedColor = colorEra(response.sport_pitching_tm.queryResults.row.era);
+                playerPitchingAverage.style.color = returedColor;
+                playerPitchingAverage.classList.add('player-era');
+            }
+            newRow.appendChild(playerPitchingAverage);
+            pitcherTable.childNodes[3].appendChild(newRow);
+        });
+    });
+}
+
+// Obtain Player Statistics
+function obtainPitchingStats(playerId) {
+
+    return fetch(`http://lookup-service-prod.mlb.com/json/named.sport_pitching_tm.bam?league_list_id='mlb'&game_type='R'&season='2021'&player_id='${playerId}'`)
+    .then(response => {
+        if(response.status !== 200) {
+            console.log('Something went wrong');
+        } else {
+            return response.json()
+        }
+    })
+    .then(data => {
+        console.log(playerId, data);
+        return data;
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
+}
+
+// Function to execute on click
+function pitcherDetails(e) {
+    // Get player ID
+    let selectedPlayer;
+    let selectedPlayerName;
+    if (e.target.parentElement.getAttribute('data-playerid') === null) {
+        selectedPlayer = e.target.parentElement.parentElement.getAttribute('data-playerid');
+    } else {
+        selectedPlayer = e.target.parentElement.getAttribute('data-playerid');
+        console.log('selected player else', selectedPlayer);
+        selectedPlayerName = e.target.parentElement.childNodes[0].innerText;
+    }
+
+    // Obtain 40 Man Roster Stats
+
+    // Obtain Advanced Pitching Stats
+    obtainPitchingStats(selectedPlayer).then(response => {
+        console.log('response', response);
+        selectedPlayerName = response.sport_pitching_tm.queryResults.row;
+    });
+    
+    // Create Modal
+    let modal = document.getElementById('modal');
+    modal.classList.add('modal-active');
+    let modalInformation = document.createElement('div');
+    modalInformation.classList.add('modal-information');
+    modal.appendChild(modalInformation);
+
+    modalInformation.innerHTML = `
+        <h1>${selectedPlayerName}</h1>
+    `;
+
+    listenForClose();
+}
+
+// Event Listener to back out of modal mode
+function listenForClose() {
+    console.log('2 seconds have passed');
+    document.querySelector('body').addEventListener('click', e => {
+        let clickedTarget = e.target.className;
+        switch(clickedTarget) {
+            case 'modal-active':
+                document.getElementById('modal').classList.remove('modal-active');
+                document.querySelector('.modal-information').remove();
+                break;
+            default:
+                console.log('not success');
+        }
+    })
+}
+
+// Determined color of ERA
+function colorEra(eraString) {
+    let era = parseFloat(eraString);
+    switch (true) {
+        case era <= 2.00:
+            return 'Darkgreen';
+        case era > 2.00 && era <= 3.00:
+            return 'Green';
+        case era > 3.00 && era <= 4.00:
+            return 'Lightgreen';
+        case era > 4.00 && era <= 5.00:
+            return 'Orange';
+        case era > 5.00 && era <= 6.00:
+            return 'orangered';
+        case era > 6.00:
+            return 'red';
+        default:
+            console.log('Result Defaulted... Something went wrong');
+    }
+
+}
---
+++ script/mapdata.js
@@ -0,0 +1,343 @@
+var simplemaps_usmap_mapdata={
+  main_settings: {
+   //General settings
+    width: "750", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    
+    //Label defaults
+    label_color: "#002767",
+    hide_labels: "no",
+    border_color: "white",
+    
+    //State defaults
+    state_description: "Learn about the products available in your state.",
+    state_color: "#77bcff",
+    state_hover_color: "#038EEB",
+    state_url: "http://simplemaps.com",
+    all_states_inactive: "no",
+    
+    //Location defaults
+    location_description: "Field Office",
+    location_color: "#ff46a2",
+    location_pulse: "yes",
+    location_pulse_size: "3",
+    location_opacity: ".8",
+    location_url: "http://simplemaps.com",
+    all_states_zoomable: "yes",
+    location_size: "40",
+    location_type: "marker",
+    all_locations_inactive: "no",
+    url_new_tab: "yes",
+    auto_load: "yes",
+   
+    //Zoom settings
+    zoom: "yes",
+    initial_zoom: "-1",
+    initial_zoom_solo: "yes",
+    
+    //Advanced settings
+    div: "map",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: "",
+    label_hover_color: "",
+    label_size: "",
+    label_font: "",
+    popups: "detect"
+  },
+  state_specific: {
+    HI: {
+      name: "Hawaii",
+      hide: "no",
+      inactive: "yes"
+    },
+    AK: {
+      name: "Alaska",
+      hide: "no",
+      inactive: "yes"
+    },
+    FL: {
+      name: "Florida",
+      inactive: "yes",
+      hide: "no"
+    },
+    NH: {
+      name: "New Hampshire",
+      hide: "no",
+      inactive: "yes"
+    },
+    VT: {
+      name: "Vermont",
+      hide: "no",
+      inactive: "yes"
+    },
+    ME: {
+      name: "Maine",
+      hide: "no",
+      inactive: "yes"
+    },
+    RI: {
+      name: "Rhode Island",
+      hide: "no",
+      inactive: "yes"
+    },
+    NY: {
+      name: "New York",
+      hide: "no",
+      inactive: "yes"
+    },
+    PA: {
+      name: "Pennsylvania",
+      hide: "no",
+      inactive: "yes"
+    },
+    NJ: {
+      name: "New Jersey",
+      hide: "no",
+      inactive: "yes"
+    },
+    DE: {
+      name: "Delaware",
+      hide: "no",
+      inactive: "yes"
+    },
+    MD: {
+      name: "Maryland",
+      hide: "no",
+      inactive: "yes"
+    },
+    VA: {
+      name: "Virginia",
+      hide: "no",
+      inactive: "yes"
+    },
+    WV: {
+      name: "West Virginia",
+      hide: "no",
+      inactive: "yes"
+    },
+    OH: {
+      name: "Ohio",
+      hide: "no",
+      inactive: "yes"
+    },
+    IN: {
+      name: "Indiana",
+      hide: "no",
+      inactive: "yes"
+    },
+    IL: {
+      name: "Illinois",
+      hide: "no",
+      inactive: "yes"
+    },
+    CT: {
+      name: "Connecticut",
+      hide: "no",
+      inactive: "yes"
+    },
+    WI: {
+      name: "Wisconsin",
+      hide: "no",
+      inactive: "yes"
+    },
+    NC: {
+      name: "North Carolina",
+      hide: "no",
+      inactive: "yes"
+    },
+    DC: {
+      name: "District of Columbia",
+      hide: "no",
+      inactive: "yes"
+    },
+    MA: {
+      name: "Massachusetts",
+      hide: "no",
+      inactive: "yes"
+    },
+    TN: {
+      name: "Tennessee",
+      hide: "no",
+      inactive: "yes"
+    },
+    AR: {
+      name: "Arkansas",
+      hide: "no",
+      inactive: "yes"
+    },
+    MO: {
+      name: "Missouri",
+      hide: "no",
+      inactive: "yes"
+    },
+    GA: {
+      name: "Georgia",
+      hide: "no",
+      inactive: "yes"
+    },
+    SC: {
+      name: "South Carolina",
+      hide: "no",
+      inactive: "yes"
+    },
+    KY: {
+      name: "Kentucky",
+      hide: "no",
+      inactive: "yes"
+    },
+    AL: {
+      name: "Alabama",
+      hide: "no",
+      inactive: "yes"
+    },
+    LA: {
+      name: "Louisiana",
+      hide: "no",
+      inactive: "yes"
+    },
+    MS: {
+      name: "Mississippi",
+      hide: "no",
+      inactive: "yes"
+    },
+    IA: {
+      name: "Iowa",
+      hide: "no",
+      inactive: "yes"
+    },
+    MN: {
+      name: "Minnesota",
+      hide: "no",
+      inactive: "yes"
+    },
+    OK: {
+      name: "Oklahoma",
+      hide: "no",
+      inactive: "yes"
+    },
+    TX: {
+      name: "Texas",
+      hide: "no",
+      inactive: "yes"
+    },
+    NM: {
+      name: "New Mexico",
+      hide: "no",
+      inactive: "yes"
+    },
+    KS: {
+      name: "Kansas",
+      hide: "no",
+      inactive: "yes"
+    },
+    NE: {
+      name: "Nebraska",
+      hide: "no",
+      inactive: "yes"
+    },
+    SD: {
+      name: "South Dakota",
+      hide: "no",
+      inactive: "yes"
+    },
+    ND: {
+      name: "North Dakota",
+      hide: "no",
+      inactive: "yes"
+    },
+    WY: {
+      name: "Wyoming",
+      hide: "no",
+      inactive: "yes"
+    },
+    MT: {
+      name: "Montana",
+      hide: "no",
+      inactive: "yes"
+    },
+    CO: {
+      name: "Colorado",
+      hide: "no",
+      inactive: "yes"
+    },
+    UT: {
+      name: "Utah",
+      hide: "no",
+      inactive: "yes"
+    },
+    AZ: {
+      name: "Arizona",
+      hide: "no",
+      inactive: "yes"
+    },
+    NV: {
+      name: "Nevada",
+      hide: "no",
+      inactive: "yes"
+    },
+    OR: {
+      name: "Oregon",
+      hide: "no",
+      inactive: "yes"
+    },
+    WA: {
+      name: "Washington",
+      hide: "no",
+      inactive: "yes"
+    },
+    CA: {
+      name: "California",
+      hide: "no",
+      inactive: "yes"
+    },
+    MI: {
+      name: "Michigan",
+      hide: "no",
+      inactive: "yes"
+    },
+    ID: {
+      name: "Idaho",
+      hide: "no",
+      inactive: "yes"
+    },
+    GU: {
+      name: "Guam",
+      hide: "yes",
+      inactive: "yes"
+    },
+    VI: {
+      name: "Virgin Islands",
+      hide: "yes",
+      inactive: "yes"
+    },
+    PR: {
+      name: "Puerto Rico",
+      hide: "yes",
+      inactive: "yes"
+    },
+    MP: {
+      name: "Northern Mariana Islands",
+      hide: "yes",
+      inactive: "yes"
+    }
+  },
+  locations: {
+    "8": {
+      name: "Minneapolis, MN",
+      lat: "45.0059",
+      lng: "-93.4305",
+      description: "Minnesota Twins",
+      size: "50",
+      type: "circle",
+      color: "#ffe42f",
+      url: "../twins.html"
+    }
+  },
+  regions: {},
+  labels: {},
+  legend: {
+    entries: []
+  }
+};
